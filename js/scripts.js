8bdcfee143f5788babbb659b62ec5186f59e8948
Beryl Onyancha <berylonyancha@gmail>
2019-05-13 16:13:26 +0300

    work on bugs

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  $(".loader").toggle(10000);
   $(".showing").click(function() {
     $(this).children(".instructions").toggle();
     $(this).children("#ins").toggle();
@@ -9,19 +10,15 @@ function myFunction(){
   var die1 = document.getElementById("die1");
   var d1 = Math.floor(Math.random() * 6) +1;
   die1.innerHTML = d1;
+  if (numbers=== 1){
+    alert("Turn over! You've rolled 1")
+  }
 };
  function myFunction2(){
   var die2 = document.getElementById("die2");
   var d2 = Math.floor(Math.random() * 6) +1;
   die2.innerHTML = d2;
  };
- $("hold").click(function (event){
-   event.preventDefault();
-   player1.active = false;
-   player2.active = true;
-   player1.hold();
-   $(".total").text(player1.total);
-   player1.roll = 0
-   
- })
+
+
  
